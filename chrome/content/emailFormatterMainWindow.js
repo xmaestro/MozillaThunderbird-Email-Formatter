b741a69240e8201963793e9e6bd092acc6d4b52d
Rizwan Hussain <rizwan.niche@nxvt.com>
2014-12-03 15:24:23 +0500

    Email window code added

--- chrome/content/emailFormatterMainWindow.js
+++ chrome/content/emailFormatterMainWindow.js
@@ -1,4 +1,10 @@
-window.addEventListener("load", function(e) { 
+var {Cc, Ci} = require("chrome");
+var Application = Cc["@mozilla.org/steel/application;1"]
+    .getService(Ci.steelIApplication);
+
+Components.utils.import("resource:///modules/MailUtils.js");
+
+window.addEventListener("load", function(e) {
 	 
 }, false);
 
@@ -11,4 +17,89 @@ function addNewTask(objid){
         parentVBox.appendChild(input);
         
         
+}
+
+function nodeIterator(objid){
+
+        var _mainNode = document.getElementById(objid).childNodes;
+
+        var itemsToReturn = [];
+
+        for(var i=0; i<_mainNode.length; i++){
+
+                itemsToReturn.push(_mainNode[i].value);
+
+        }
+
+        return itemsToReturn;
+
+}
+
+function getFormData(){
+
+        var emailsTo = [];
+        var emailsCc = [];
+        var tasksToday = [];
+        var tasksInProgress = [];
+        var tasksTomorrow = [];
+
+        emailsTo = nodeIterator('emailformatter-emailstobox');
+        emailsCc = nodeIterator('emailformatter-emailsccbox');
+        tasksToday = nodeIterator('emailformatter-todaytasksvbox');
+        tasksInProgress = nodeIterator('emailformatter-tasksinprogress');
+        tasksTomorrow = nodeIterator('emailformatter-tomorrowtasksbox');
+
+
+        return {
+
+                emailsTo: emailsTo,
+                emailsCc: emailsCc,
+                tasksToday:tasksToday,
+                tasksInProgress:tasksInProgress,
+                tasksTomorrow:tasksTomorrow
+
+        };
+
+};
+
+function openMailWindow(){
+
+        var data = getFormData();
+
+        var emailsToString="";
+
+        for(var emailsTo in data.emailsTo){
+
+                emailsToString+=data.emailsTo[emailsTo]+"<"+data.emailsTo[emailsTo]+">,";
+
+
+        }
+
+        var emailsCcString="";
+
+        for(var emailsCc in data.emailsCc){
+
+                emailsCcString+=data.emailsCc[emailsCc]+"<"+data.emailsCc[emailsCc]+">,";
+
+
+        }
+
+        var today = new Date();
+
+        var sURL="mailto:"+encodeURI(emailsToString)+"?"+"subject="+encodeURI('DAILY STATUS REPORT ('+today.getFullYear()+'-'+today.getMonth()+'-'+today.getDate()+')')+"&cc="+encodeURI(emailsCcString);
+
+        var msgComposeService=
+            Components.classes["@mozilla.org/messengercompose;1"]
+                .getService(Components.interfaces.nsIMsgComposeService);
+
+
+        var ioService =
+            Components.classes["@mozilla.org/network/io-service;1"]
+                .getService(Components.interfaces.nsIIOService);
+
+        aURI = ioService.newURI(sURL, null, null);
+
+        msgComposeService.OpenComposeWindowWithURI (null, aURI);
+
+
 }
